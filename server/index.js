cf24bc14134d69671fe09b6fe5c8e215a023b811
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-05-15 17:17:28 -0700

    Update: port to 80 from 8080

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const { db } = require("./db");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 const app = require("./app");
 const seed = require("../script/seed");
 
